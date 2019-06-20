819b36848618f5d48319fd1cb13c5c03656d51d3
Cris Ronda <cristian.ronda@epn.edu.ec>
2019-06-19 23:44:10 -0500

    Resize photo

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-const functions = require('firebase-functions');
-const { Storage } = require('@google-cloud/storage');
-const projectId = 'restaurantrn-d53d2'
-let gcs = new Storage({ projectId })
-const os = require('os')
-const path = require('path')
+const functions = require("firebase-functions");
+const { Storage } = require("@google-cloud/storage");
+const projectId = "restaurantrn-d53d2";
+let gcs = new Storage({ projectId });
+const os = require("os");
+const path = require("path");
+const spawn = require("child-process-promise").spawn;
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -11,22 +12,29 @@ const path = require('path')
 //  response.send("Hello from Firebase!");
 // });
 exports.onFileChange = functions.storage.object().onFinalize(event => {
-    console.log(event)
-    const bucket = event.bucket;
-    const contentType = event.contentType;
-    const filePath = event.name;
-    console.log("Se subio la imagen, procesando ....")
-    if (path.basename(filePath).startsWith('renombrado_')) {
-        console.log('Ya se renombro la imagen')
-        return;
-    }
-    const destBucket = gcs.bucket(bucket);
-    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath))
-    const metadata = { contentType }
-    return destBucket.file(filePath).download({ destination: tmpFilePath }).then(() => {
-        return destBucket.upload(tmpFilePath, {
-            destination: 'renombrado_' + path.basename(filePath),
-            metadata
-        })
+  console.log(event);
+  const bucket = event.bucket;
+  const contentType = event.contentType;
+  const filePath = event.name;
+  console.log("Se subio la imagen, procesando ....");
+
+  if (path.basename(filePath).startsWith("resized_")) {
+    console.log("Ya se renombro la imagen");
+    return;
+  }
+  const destBucket = gcs.bucket(bucket);
+  const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+  const metadata = { contentType };
+  return destBucket
+    .file(filePath)
+    .download({ destination: tmpFilePath })
+    .then(() => {
+      return spawn("convert", [tmpFilePath, "-resize", "500x500", tmpFilePath]);
     })
-})
+    .then(() => {
+      return destBucket.upload(tmpFilePath, {
+        destination: "resized_" + path.basename(filePath),
+        metadata
+      });
+    });
+});
