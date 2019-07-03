0afa630579853a3dea8421f6a9f9414fbc9e335e
Cris Ronda <cristian.ronda@epn.edu.ec>
2019-07-03 12:54:29 -0500

    Primer commit

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,25 @@
 const functions = require("firebase-functions");
 const { Storage } = require("@google-cloud/storage");
 const projectId = "restaurantrn-d53d2";
+const admin = require("firebase-admin");
 let gcs = new Storage({ projectId });
 const os = require("os");
 const path = require("path");
 const spawn = require("child-process-promise").spawn;
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 exports.onFileChange = functions.storage.object().onFinalize(event => {
   console.log(event);
   const bucket = event.bucket;
   const contentType = event.contentType;
   const filePath = event.name;
+  const pathImage = path.basename(filePath);
   console.log("Se subio la imagen, procesando ....");
 
-  if (path.basename(filePath).startsWith("resized_")) {
+  if (pathImage.startsWith("resized_")) {
     console.log("Ya se renombro la imagen");
     return;
   }
   const destBucket = gcs.bucket(bucket);
-  const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+  const tmpFilePath = path.join(os.tmpdir(), pathImage);
   const metadata = { contentType };
   return destBucket
     .file(filePath)
@@ -33,8 +29,47 @@ exports.onFileChange = functions.storage.object().onFinalize(event => {
     })
     .then(() => {
       return destBucket.upload(tmpFilePath, {
-        destination: "resized_" + path.basename(filePath),
+        destination: "resized_" + pathImage,
         metadata
       });
+    })
+    .then(() => {
+      return destBucket.file(filePath).delete();
     });
+});
+
+//Loop por reparar la ruta deberia actualizase cuando un user da like a una
+exports.onUpdateLikes = functions.database
+  .ref("/data/{id}/usersLikes")
+  .onUpdate(async change => {
+    const { before, after } = change;
+    console.log("Hola desde el update!!");
+    console.log("ANTES", before.val());
+    console.log("DESPUES", after.val());
+    const refLikes = after.ref.parent.child("likes");
+    console.log(refLikes);
+    refLikes.transaction(count => {
+      console.log(count);
+      return count + 1;
+    });
+  });
+
+exports.myOwnFunction = functions.https.onRequest((req, res) => {
+  console.log(req);
+  console.log(res);
+  if (req.method === "POST") {
+    return res.status(500).json({
+      message: "NO PERMITIDO"
+    });
+  }
+  admin
+    .database()
+    .ref("/data/")
+    .on("value", snap => {
+      return res.status(200).json({
+        message: "FUNCIONA",
+        data: snap.val()
+      });
+    });
+  return 0;
 });
